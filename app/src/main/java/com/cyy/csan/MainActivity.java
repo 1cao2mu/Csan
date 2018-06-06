1e2f25e88841c5cb2a791598a1ce1a6b0b8eb133
caoyangyang <2451772768@qq.com>
2018-06-06 19:56:41 +0800

    aaa

--- app/src/main/java/com/cyy/csan/MainActivity.java
+++ app/src/main/java/com/cyy/csan/MainActivity.java
@@ -24,20 +24,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        mCityList.add(new City("凉州",10,10));
-        mCityList.add(new City("益州",10,70));
-        mCityList.add(new City("雍州",30,50));
-        mCityList.add(new City("司隶",50,50));
-        mCityList.add(new City("荆州",50,70));
-        mCityList.add(new City("并州",60,20));
-        mCityList.add(new City("幽州",80,10));
-        mCityList.add(new City("冀州",70,30));
-        mCityList.add(new City("青州",80,40));
-        mCityList.add(new City("徐州",80,50));
-        mCityList.add(new City("扬州",70,70));
-        mCityList.add(new City("交州",60,80));
-        mCityList.add(new City("兖州",50,60));
-        mCityList.add(new City("豫州",60,60));
+        mCityList.add(new City("凉州",15,15));
+        mCityList.add(new City("益州",15,65));
+        mCityList.add(new City("雍州",25,40));
+        mCityList.add(new City("司隶",40,40));
+        mCityList.add(new City("荆州",40,65));
+        mCityList.add(new City("并州",52.5f,20));
+        mCityList.add(new City("幽州",72.5f,10));
+        mCityList.add(new City("冀州",65f,25));
+        mCityList.add(new City("青州",80f,32.5f));
+        mCityList.add(new City("徐州",77.5f,45));
+        mCityList.add(new City("扬州",67.5f,62.5f));
+        mCityList.add(new City("交州",57.5f,80));
+        mCityList.add(new City("兖州",60f,40));
+        mCityList.add(new City("豫州",60f,52.5f));
         sm_map = findViewById(R.id.sm_map);
         sm_map.setCityList(mCityList);
     }
